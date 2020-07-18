b1075238eb042b812e990e7bef2ff5a7db88e63c
zz1044063894 <985038432@qq.com>
2020-07-18 16:11:29 +0800

    博客：https://blog.csdn.net/weixin_43876121/article/details/107412709 JMM特性源码

--- src/com/jingchu/juc/jmm/OrderExample.java
+++ src/com/jingchu/juc/jmm/OrderExample.java
@@ -9,33 +9,29 @@ package com.jingchu.juc.jmm;
 public class OrderExample {
     public static void main(String[] args) {
         MyOrderData myData = new MyOrderData();
-        new Thread(()->{
-            try {
-                myData.write();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        },"AAA").start();
-        new Thread(()->{
+        new Thread(() -> {
+            myData.write();
+        }, "AAA").start();
+        new Thread(() -> {
             myData.read();
-        },"BBB").start();
+        }, "BBB").start();
     }
 }
-class MyOrderData{
+
+class MyOrderData {
     int a = 0;
     boolean flag = false;
 
-    public void write() throws InterruptedException {
-        flag = true;
-        Thread.sleep(2000);
+    public void write()  {
         a = 1;
-        System.out.println(Thread.currentThread().getName()+" "+a);
+        flag = true;
+        System.out.println(Thread.currentThread().getName() + " " + a);
     }
 
     public void read() {
         if (flag) {
-
-            System.out.println(Thread.currentThread().getName()+" "+a);
+            a += 5;
+            System.out.println(Thread.currentThread().getName() + " " + a);
         }
     }
 }
